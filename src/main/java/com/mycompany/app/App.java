a6c4dce13b335276057105e3676ca276367a9688
TavOexe <91002464+TavOexe@users.noreply.github.com>
2021-12-02 17:25:21 -0500

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App
 {
-
+    private final String points = "i deserve 20 points"
     private final String message = "Hello World!";
 
     public App() {}
